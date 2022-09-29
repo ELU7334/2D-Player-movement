0e6de4975854863cd4ef9aa5dc44c69ca2293392
ELU7334 <111079531+ELU7334@users.noreply.github.com>
2022-09-29 11:02:35 -0700

    Ridged body movement and script updates

---
+++ 2D Player Movement/Assets/script/Ridged2DMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ridged2DMovement : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    public float jumpForce = 10;
+    
+    public bool isOnGround;
+
+    private Rigidbody2D _myRB;
+    private Collider2D _myCpllider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _myRB = GetComponent<Rigidbody2D>();
+        _myCpllider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+
+        _myRB.velocity = new Vector2(horizontalInput * moveSpeed, _myRB.velocity.y);
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            _myRB.velocity = new Vector2(_myRB.velocity.x, jumpForce);
+        }
+
+    }
+}
