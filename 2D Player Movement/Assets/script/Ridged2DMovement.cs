f2d295799169300263f3d929db957606992543c9
ELU7334 <111079531+ELU7334@users.noreply.github.com>
2022-09-30 11:14:05 -0700

    updates of script

--- 2D Player Movement/Assets/script/Ridged2DMovement.cs
+++ 2D Player Movement/Assets/script/Ridged2DMovement.cs
@@ -21,15 +21,33 @@ public class Ridged2DMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+      PlayerMovement();
+      PlayerJump();
+
+    }
+    void PlayerMovement()
+        {
+             
         float horizontalInput = Input.GetAxis("Horizontal");
 
 
         _myRB.velocity = new Vector2(horizontalInput * moveSpeed, _myRB.velocity.y);
 
-        if(Input.GetButtonDown("Jump"))
+        }
+
+        void PlayerJump()
+        {
+            if(_myCpllider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+            {
+                isOnGround = true;
+            }
+            else{
+                isOnGround = false;
+            }
+               if(Input.GetButtonDown("Jump") && isOnGround)
         {
             _myRB.velocity = new Vector2(_myRB.velocity.x, jumpForce);
         }
-
-    }
+        }
+    
 }
